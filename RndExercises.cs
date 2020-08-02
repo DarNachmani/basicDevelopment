b43c20d5b1de889b8552d34b1d276ac90abc3acf
Dar Nachmani <dar.nachmani@gmail.com>
2020-08-02 21:56:31 +0300

    finished most of the exercises

--- RndExercises.cs
+++ RndExercises.cs
@@ -83,6 +83,105 @@ public class RndExercises
 
     public Dictionary<string, int> Ex5(List<string> strs)
     {
-        
+        Dictionary<string, int> countOfStrings = new Dictionary<string, int>();
+        foreach(var str in strs)
+        {
+            if(countOfStrings.Keys.Contains(str))
+            {
+                countOfStrings[str] ++;
+            }
+            else
+            {
+                countOfStrings[str] = 1;
+            }
+        }
+        return countOfStrings;
+    }
+
+    public void DifferenceBetweenMainElement(int[] arr)
+    {
+        int mainElement = 13;
+        for(int i=0; i<arr.Length; i++)
+        {
+            if(i != mainElement)
+            {
+                Console.WriteLine(Math.Abs(mainElement - arr[i]));
+            }
+        }
+    }
+
+    public int OccuranceOfTatInStr(string str, string tat)
+    {
+        int index = 0;
+        int i = 0;
+        int count = 0;
+        while(index != -1)
+        {
+            index = str.IndexOf(tat, i);
+            i += tat.Length;
+            count ++;
+        }
+
+        return count;
+    }
+
+    public int Metric(int[,] arr, int rowNum1, int rowNum2)
+    {
+        int sumOfFirstRow = 0;
+        int sumOfSecondRow = 0;
+        for(int i=0; i<arr.GetLength(1); i++)
+        {
+            sumOfFirstRow += arr[rowNum1, i];
+            sumOfSecondRow += arr[rowNum2, i];
+        }
+        if(sumOfFirstRow == sumOfSecondRow)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    public void BuildArray(int n)
+    {
+        var rnd = new Random();
+        int countOfInputs = 0;
+        int[] arr = new int[n];
+        int doubleIndex = 0;
+        int oddIndex = 1;
+        while(countOfInputs < n)
+        {
+            rndNum = rnd.Next(10, 100);
+            if(rndNum % 2 == 0)
+            {
+                if(doubleIndex < arr.Length - 2)
+                {
+                    arr[doubleIndex] = rndNum;
+                    doubleIndex += 2;
+                    countOfInputs ++;
+                }
+            }
+            else
+            {
+                if(oddIndex < arr.Length - 2)
+                {
+                    arr[oddIndex] = rndNum;
+                    oddIndex += 2;
+                    countOfInputs ++;
+                }
+            }
+        }
+    }
+
+    public int StringLengthEqualsToNum(string[] arr, int num)
+    {
+        int count = 0;
+        foreach(var str in arr)
+        {
+            if(num == str.Length)
+            {
+                count ++;
+            }
+        }
+        return count;
     }
 }
