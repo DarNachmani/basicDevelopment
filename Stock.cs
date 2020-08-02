b7927819b2b8a25b28e57ad0397d209f2ac31e8d
Dar Nachmani <dar.nachmani@gmail.com>
2020-08-02 18:47:31 +0300

    finished Stock - need to check things

--- Stock.cs
+++ Stock.cs
@@ -2,26 +2,34 @@
 
 public class Stock
 {
-	public List<Tuple<int, int>> inventory;
+	public Tuple<int, int> [] inventory;
 	public int countOfMasks, profit;
 
 	public Stock(int countOfMasksSold)
 	{
-		this.inventory = new List<Tuple<int, int>>();
+		this.inventory = new Tuple<int, int> [5];
 		this.countOfMasksSold = countOfMasksSold;
 		this.profit = 0;
 	}
 
 	public void Buy(int quantity, double price)
     {
+		double tempQuantity = 0;
 		foreach(var mask in this.inventory)
         {
-			if(mask[1] == price)
-            {
-				mask[0] -= quantity;
-				this.profit = quantity * price;
-				this.countOfMasksSold += quantity;
-            }
+			while(quantity > 0){
+				if(mask.Item1 >= quantity)
+				{
+					mask.Item1 -= quantity;
+					this.profit = quantity * price;
+					this.countOfMasksSold += quantity;
+					quantity = 0;
+				}
+				else{
+					quantity -= mask.Item1;
+					mask.Item1 = 0;
+				}
+			}
 		}
 		
     }
@@ -32,18 +40,20 @@ public class Stock
 		double tempProfit = 0.0;
 		foreach (var mask in this.inventory)
         {
-			if(mask[0] >= quantity)
-            {
-				double cost = mask[1] * quantity + tempCost;
-				double profit = quantity * price + tempProfit;
-				return profit - cost;
-            }
-            else
-            {
-				tempCost = mask[0] * mask[1];
-				tempProfit = mask[0] * price;
-				quantity -= mask[0];
-            }
+			while(quantity > 0){
+					if(mask[0] >= quantity)
+				{
+					double cost = mask.Item2 * quantity + tempCost;
+					double profit = quantity * price + tempProfit;
+					return profit - cost;
+				}
+				else
+				{
+					tempCost = mask.Item1 * mask.Item2;
+					tempProfit = mask.Item1 * price;
+					quantity -= mask.Item1;
+				}
+			}
         }
     }
 
@@ -54,6 +64,10 @@ public class Stock
 
 	public string ToString()
     {
-		return string.Format("From stock number {}");
+		string finalString = "";
+		for(int i=0; i<5; i++){
+			finalString += string.Format("From stock number {0} there are {1} masks", i+1, this.inventory[i].Item1);
+		}
+		return finalString;
     }
 }
