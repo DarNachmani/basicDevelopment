4c9f5d8779885b5657f5436fdd21e2013c49763e
Dar Nachmani <dar.nachmani@gmail.com>
2020-08-05 10:33:03 +0300

    fixing

--- Stock.cs
+++ Stock.cs
@@ -2,49 +2,55 @@
 
 public class Stock
 {
-	public Tuple<int, int> [] inventory;
-	public int countOfMasks, profit;
+	public Tuple<int, double>[] inventory;
+	public int countOfMasksSold;
+	public double profit;
 
-	public Stock(int countOfMasksSold)
+	public Stock(Tuple<int, double>[] arr)
 	{
-		this.inventory = new Tuple<int, int> [5];
-		this.countOfMasksSold = countOfMasksSold;
+		this.inventory = arr;
+		this.countOfMasksSold = 0;
 		this.profit = 0;
 	}
 
 	public void Buy(int quantity, double price)
-    {
+	{
 		double tempQuantity = 0;
-		foreach(var mask in this.inventory)
-        {
-			while(quantity > 0){
-				if(mask.Item1 >= quantity)
+		for (int i = 0; i < this.inventory.Length; i++)
+		{
+			var mask = this.inventory[i];
+			while (quantity > 0)
+			{
+				if (mask.Item1 >= quantity)
 				{
-					mask.Item1 -= quantity;
+					this.inventory[i] = Tuple.Create(mask.Item1 - quantity, mask.Item2);
 					this.profit = quantity * price;
 					this.countOfMasksSold += quantity;
 					quantity = 0;
 				}
-				else{
+				else
+				{
 					quantity -= mask.Item1;
-					mask.Item1 = 0;
+					this.inventory[i] = Tuple.Create(0, mask.Item2);
 				}
 			}
 		}
-		
-    }
+	}
 
 	public double Sell(int quantity, double price)
-    {
+	{
 		double tempCost = 0.0;
 		double tempProfit = 0.0;
+		double cost = 0;
+		double profit = 0;
 		foreach (var mask in this.inventory)
-        {
-			while(quantity > 0){
-					if(mask[0] >= quantity)
+		{
+			while (quantity > 0)
+			{
+				if (mask.Item1 >= quantity)
 				{
-					double cost = mask.Item2 * quantity + tempCost;
-					double profit = quantity * price + tempProfit;
+					cost = mask.Item2 * quantity + tempCost;
+					profit = quantity * price + tempProfit;
 					return profit - cost;
 				}
 				else
@@ -54,20 +60,23 @@ public class Stock
 					quantity -= mask.Item1;
 				}
 			}
-        }
-    }
+			return profit - cost;
+		}
+		return profit - cost;
+	}
 
 	public int NumOfSold()
-    {
+	{
 		return this.countOfMasksSold;
-    }
+	}
 
 	public string ToString()
-    {
+	{
 		string finalString = "";
-		for(int i=0; i<5; i++){
-			finalString += string.Format("From stock number {0} there are {1} masks", i+1, this.inventory[i].Item1);
+		for (int i = 0; i < 5; i++)
+		{
+			finalString += string.Format("From stock number {0} there are {1} masks", i + 1, this.inventory[i].Item1);
 		}
 		return finalString;
-    }
+	}
 }
